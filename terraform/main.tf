800e7597d36ab054ab8dc0a775e6ad5de57a5884
Samuel Cadavid <samuelcadavid@Samuels-MacBook-Pro.local>
2025-03-02 19:09:17 -0500

    network name tags

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,22 +3,28 @@ variable "aws_region" {
   description = "The AWS region to deploy to"
   type        = string
   default     = "us-east-1"
-  
+
 }
 
 #-------------------------
 # VPC & Subnets
 #-------------------------
 resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
-  enable_dns_support = true
+  enable_dns_support   = true
+  tags = {
+    Name = "Demo VPC"
+  }
 }
 
 resource "aws_subnet" "private_a" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = "us-east-1a"
+  tags = {
+    Name = "Private Subnet A"
+  }
 
 }
 
@@ -26,25 +32,36 @@ resource "aws_subnet" "private_b" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.2.0/24"
   availability_zone = "us-east-1b"
-  # map_public_ip_on_launch = true
+  tags = {
+    Name = "Private Subnet B"
+  }
 }
 
 resource "aws_subnet" "public_a" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.3.0/24"
-  availability_zone = "us-east-1a"
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.3.0/24"
+  availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
+  tags = {
+    Name = "Public Subnet A"
+  }
 }
 
 resource "aws_subnet" "public_b" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.4.0/24"
-  availability_zone = "us-east-1b"
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.4.0/24"
+  availability_zone       = "us-east-1b"
   map_public_ip_on_launch = true
+  tags = {
+    Name = "Public Subnet B"
+  }
 }
 
 resource "aws_internet_gateway" "main" {
-  vpc_id = aws_vpc.main.id   
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "Demo IGW"
+  }
 }
 
 resource "aws_route_table" "public" {
@@ -52,7 +69,11 @@ resource "aws_route_table" "public" {
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.main.id  
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "Public Route Table"
   }
 
 }
@@ -60,39 +81,50 @@ resource "aws_route_table" "public" {
 resource "aws_route_table_association" "public_a" {
   subnet_id      = aws_subnet.public_a.id
   route_table_id = aws_route_table.public.id
-  
+
 }
 
 resource "aws_route_table_association" "public_b" {
   subnet_id      = aws_subnet.public_b.id
   route_table_id = aws_route_table.public.id
-  
+
 }
 
 resource "aws_eip" "ngw" {
   domain = "vpc"
-  
+
+  tags = {
+    Name = "Demo NGW EIP"
+  }
 }
 
 resource "aws_nat_gateway" "main" {
   allocation_id = aws_eip.ngw.id
   subnet_id     = aws_subnet.public_a.id
-  
+
+  tags = {
+    Name = "Demo NGW"
+  }
+
 }
 
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
 
   route {
-    cidr_block = "0.0.0.0/0"
+    cidr_block     = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.main.id
-  
-}
+
+  }
+
+  tags = {
+    Name = "Private Route Table"
+  }
 }
 resource "aws_route_table_association" "private_a" {
   subnet_id      = aws_subnet.private_a.id
   route_table_id = aws_route_table.private.id
-  
+
 }
 
 resource "aws_route_table_association" "private_b" {
