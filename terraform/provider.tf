f345d3eb567943e0d403e22a75a3585bfbc16fed
Samuel Cadavid <samuelcadavid@Samuels-MacBook-Pro.local>
2025-03-03 22:00:59 -0500

    tflint fixes:

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -4,3 +4,13 @@ provider "aws" {
   }
   region = "us-east-1"
 }
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = ">= 5.89.0"
+    }
+  }
+  required_version = ">= 1.11"
+}
