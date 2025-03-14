32609ca8fe9c9ef862c92bd7ced74444d76c9b8d
Samuel Cadavid <samuelcadavid@Samuels-MacBook-Pro.local>
2025-03-14 10:53:28 -0400

    refactor vpc and network logs

--- terraform/sg.tf
+++ terraform/sg.tf
@@ -2,7 +2,7 @@
 #-------------------------
 resource "aws_security_group" "ecs_sg" {
   name_prefix = "ecs-sg-"
-  vpc_id      = aws_vpc.main.id
+  vpc_id      = module.vpc.vpc_id
 
   ingress {
     from_port   = 80
